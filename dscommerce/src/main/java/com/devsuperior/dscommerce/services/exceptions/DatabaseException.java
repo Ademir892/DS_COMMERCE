52d7c2adbebfc97685c732ca16b2d6e1ee02df87
Ademir Traesel <ademirtraesel@hotmail.com>
2024-06-11 13:24:23 -0300

    Bean validation

--- dscommerce/src/main/java/com/devsuperior/dscommerce/services/exceptions/DatabaseException.java
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/services/exceptions/DatabaseException.java
@@ -3,7 +3,6 @@ package com.devsuperior.dscommerce.services.exceptions;
 public class DatabaseException extends RuntimeException{
 
     public DatabaseException(String msg){
-
         super(msg);
     }
 
